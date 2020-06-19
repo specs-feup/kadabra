d54b6341fc0d0dbdc7e209cd27b62e7d7f8e9274
João Bispo <joaobispo@gmail.com>
2020-06-19 02:14:35 +0100

    [JavaWeaver] Adds missing files

---
+++ JavaWeaver/src/weaver/kadabra/joinpoints/JAnnotation.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2020 SPeCS.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License. under the License.
+ */
+
+package weaver.kadabra.joinpoints;
+
+import spoon.reflect.declaration.CtAnnotation;
+import spoon.reflect.declaration.CtElement;
+import weaver.kadabra.abstracts.joinpoints.AAnnotation;
+
+public class JAnnotation extends AAnnotation {
+
+    private final CtAnnotation<?> annotation;
+
+    public JAnnotation(CtAnnotation<?> annotation) {
+        super(new JExpression<>(annotation));
+
+        this.annotation = annotation;
+    }
+
+    @Override
+    public CtElement getNode() {
+        return annotation;
+    }
+
+}
