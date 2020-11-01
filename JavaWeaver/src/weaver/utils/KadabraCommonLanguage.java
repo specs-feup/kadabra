f05428ac9aa9e40e7a46d3020d1d0e8199eabcfe
Gil Teixeira <up201505735@fe.up.pt>
2020-11-01 22:15:23 +0000

    [JavaWeaver] Add mapping for remaining common stmts

--- JavaWeaver/src/weaver/utils/KadabraCommonLanguage.java
+++ JavaWeaver/src/weaver/utils/KadabraCommonLanguage.java
@@ -13,11 +13,14 @@
 
 package weaver.utils;
 
-import pt.up.fe.specs.util.classmap.ClassMap;
+import pt.up.fe.specs.util.classmap.FunctionClassMap;
 import spoon.reflect.code.CtBinaryOperator;
+import spoon.reflect.code.CtBreak;
 import spoon.reflect.code.CtCase;
+import spoon.reflect.code.CtCatch;
 import spoon.reflect.code.CtConditional;
 import spoon.reflect.code.CtConstructorCall;
+import spoon.reflect.code.CtContinue;
 import spoon.reflect.code.CtExpression;
 import spoon.reflect.code.CtIf;
 import spoon.reflect.code.CtInvocation;
@@ -25,6 +28,8 @@ import spoon.reflect.code.CtLocalVariable;
 import spoon.reflect.code.CtLoop;
 import spoon.reflect.code.CtStatement;
 import spoon.reflect.code.CtSwitch;
+import spoon.reflect.code.CtThrow;
+import spoon.reflect.code.CtTry;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtCompilationUnit;
 import spoon.reflect.declaration.CtConstructor;
@@ -40,39 +45,55 @@ import weaver.kadabra.spoon.extensions.nodes.CtApp;
 
 public class KadabraCommonLanguage {
 
-	private static final ClassMap<CtElement, String> JOINPOINT_MAPPER;
+	private static final FunctionClassMap<CtElement, String> JOINPOINT_MAPPER;
 	static {
-		JOINPOINT_MAPPER = new ClassMap<>();
-//		JOINPOINT_MAPPER.put(CtFor.class, "ForJp");
-//		JOINPOINT_MAPPER.put(CtDo.class, "DoJp");
-//		JOINPOINT_MAPPER.put(CtWhile.class, "WhileJp");
-		JOINPOINT_MAPPER.put(CtBinaryOperator.class, "BinaryJp");
-		JOINPOINT_MAPPER.put(CtConditional.class, "TernaryJp");
-		JOINPOINT_MAPPER.put(CtLoop.class, "LoopJp");
-		JOINPOINT_MAPPER.put(CtCase.class, "CaseJp");
-		JOINPOINT_MAPPER.put(CtSwitch.class, "SwitchJp");
-		JOINPOINT_MAPPER.put(CtIf.class, "IfJp");
-		JOINPOINT_MAPPER.put(CtStatement.class, "StmtJp");
-		JOINPOINT_MAPPER.put(CtConstructorCall.class, "ConstructorCallJp");
-		JOINPOINT_MAPPER.put(CtConstructor.class, "ConstructorJp");
-		JOINPOINT_MAPPER.put(CtLocalVariableReference.class, "VarRefJp");
-		JOINPOINT_MAPPER.put(CtLocalVariable.class, "VarDeclJp");
-		JOINPOINT_MAPPER.put(CtParameter.class, "ParamJp");
-		JOINPOINT_MAPPER.put(CtTypeReference.class, "TypeJp");
-		JOINPOINT_MAPPER.put(CtFieldReference.class, "FieldRefJp");
-		JOINPOINT_MAPPER.put(CtField.class, "FieldJp");
-		JOINPOINT_MAPPER.put(CtInvocation.class, "MemberCallJp");
-		JOINPOINT_MAPPER.put(CtExpression.class, "ExprJp");
-		JOINPOINT_MAPPER.put(CtMethod.class, "MethodJp");
-		JOINPOINT_MAPPER.put(CtClass.class, "ClassJp");
-		JOINPOINT_MAPPER.put(CtNamedElement.class, "DeclJp");
-		JOINPOINT_MAPPER.put(CtCompilationUnit.class, "FileJp");
-		JOINPOINT_MAPPER.put(CtApp.class, "ProgramJp");
-		JOINPOINT_MAPPER.put(CtElement.class, "JoinPoint");
+		JOINPOINT_MAPPER = new FunctionClassMap<>();
+
+		JOINPOINT_MAPPER.put(CtContinue.class, node -> "ContinueJp");
+		JOINPOINT_MAPPER.put(CtBreak.class, node -> "BreakJp");
+		JOINPOINT_MAPPER.put(CtThrow.class, node -> "ThrowJp");
+		JOINPOINT_MAPPER.put(CtCatch.class, node -> "CatchJp");
+		JOINPOINT_MAPPER.put(CtTry.class, node -> "TryJp");
+//		JOINPOINT_MAPPER.put(CtFor.class,node -> "ForJp");
+//		JOINPOINT_MAPPER.put(CtDo.class,node -> "DoJp");
+//		JOINPOINT_MAPPER.put(CtWhile.class,node -> "WhileJp");
+		JOINPOINT_MAPPER.put(CtBinaryOperator.class, node -> "BinaryJp");
+		JOINPOINT_MAPPER.put(CtConditional.class, node -> "TernaryJp");
+		JOINPOINT_MAPPER.put(CtLoop.class, node -> "LoopJp");
+		JOINPOINT_MAPPER.put(CtCase.class, KadabraCommonLanguage::ctCase);
+		JOINPOINT_MAPPER.put(CtSwitch.class, node -> "SwitchJp");
+		JOINPOINT_MAPPER.put(CtIf.class, node -> "IfJp");
+		JOINPOINT_MAPPER.put(CtStatement.class, node -> "StmtJp");
+		JOINPOINT_MAPPER.put(CtConstructorCall.class, node -> "ConstructorCallJp");
+		JOINPOINT_MAPPER.put(CtConstructor.class, node -> "ConstructorJp");
+		JOINPOINT_MAPPER.put(CtLocalVariableReference.class, node -> "VarRefJp");
+		JOINPOINT_MAPPER.put(CtLocalVariable.class, node -> "VarDeclJp");
+		JOINPOINT_MAPPER.put(CtParameter.class, node -> "ParamJp");
+		JOINPOINT_MAPPER.put(CtTypeReference.class, node -> "TypeJp");
+		JOINPOINT_MAPPER.put(CtFieldReference.class, node -> "FieldRefJp");
+		JOINPOINT_MAPPER.put(CtField.class, node -> "FieldJp");
+		JOINPOINT_MAPPER.put(CtInvocation.class, node -> "MemberCallJp");
+		JOINPOINT_MAPPER.put(CtExpression.class, node -> "ExprJp");
+		JOINPOINT_MAPPER.put(CtMethod.class, node -> "MethodJp");
+		JOINPOINT_MAPPER.put(CtClass.class, node -> "ClassJp");
+		JOINPOINT_MAPPER.put(CtNamedElement.class, node -> "DeclJp");
+		JOINPOINT_MAPPER.put(CtCompilationUnit.class, node -> "FileJp");
+		JOINPOINT_MAPPER.put(CtApp.class, node -> "ProgramJp");
+		JOINPOINT_MAPPER.put(CtElement.class, node -> "JoinPoint");
 	}
 
 	public static String getJoinPointName(CtElement node) {
-		return JOINPOINT_MAPPER.get(node.getClass());
+		return JOINPOINT_MAPPER.apply(node);
+	}
+
+	@SuppressWarnings("rawtypes")
+	private static String ctCase(CtCase node) {
+
+		if (node.getCaseExpressions().isEmpty())
+			return "DefaultJp";
+
+		return "CaseJp";
+
 	}
 
 }
