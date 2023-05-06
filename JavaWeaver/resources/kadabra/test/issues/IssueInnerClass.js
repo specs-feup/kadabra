af279ba887bd9fdd6f8a2e3be0e0dec2c546ab31
João Bispo <joaobispo@gmail.com>
2023-05-06 14:08:11 +0100

    [JavaWeaver] Adds test for issue related with inner classes

---
+++ JavaWeaver/resources/kadabra/test/issues/IssueInnerClass.js
@@ -0,0 +1,31 @@
+laraImport("weaver.Query");
+const loop = Query.search("method", "sortedListOfMuscleIdsFrom")
+  .search("loop")
+  .first();
+
+println("Loop in inner class ancestors:");
+println("Parent x 1: " + loop.parent.joinPointType);
+//println(loop.parent.code);
+
+println("Parent x 2: " + loop.parent.parent.joinPointType);
+//println(loop.parent.parent.code);
+
+println("Parent x 3: " + loop.parent.parent.parent.joinPointType);
+//println(loop.parent.parent.parent.code);
+
+//println("------");
+println("Parent x 4: " + loop.parent.parent.parent.parent.joinPointType);
+//println(loop.parent.parent.parent.parent.code);
+
+//const $loop = Query.search("method", "getNewId").search("loop").first();
+//println("LOOP: " + $loop.code);
+//getNewId.ancestor("")
+
+/*
+laraImport("kadabra.analysis.energy.EnergyAwareAndroidPatterns");
+
+let result = new InternalGetterDetector().analyse();
+println(result.results[0].ancestor("method")); //undefined
+println(result.results[0].ancestor("class").parent); //inner class
+println(result.results[0].ancestor("class").ast); //file node
+*/
