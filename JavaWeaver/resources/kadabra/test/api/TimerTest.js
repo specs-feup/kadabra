4d4a34d22260518c55c8d41ed851f0461f6c4e4b
DiogoMartins888 <up202108883@up.pt>
2025-03-12 11:55:30 +0000

    [JavaWeaver] Translate TimerTest from lara to js

--- JavaWeaver/resources/kadabra/test/api/TimerTest.js
+++ JavaWeaver/resources/kadabra/test/api/TimerTest.js
@@ -1,50 +1,40 @@
-import lara.code.Timer;
-import lara.code.Logger;
+laraImport("lara.code.Timer");
+laraImport("lara.code.Logger");
+laraImport("weaver.Query");
 
-aspectdef TimerTest
-	
+// Instrument call to 'Calculate'
+const timer = new Timer();
 
-	// Instrument call to 'Calculate'
-	var timer = new Timer();
+for (const call of Query.search("type").search("executable").search("call")) {
+    if (call.name !== "bar" && call.name !== "foo") {
+        continue;
+    }
 
-	select type.executable.call end
-	apply
-		if($call.name !== "bar" && $call.name !== "foo") {
-			continue;
-		}
-		
-		timer.time($call, "Time:");
-	end
-	
-	// Disable printing result
-	timer.setPrint(false);
-	
-	select type.executable.call{"bar2"} end
-	apply
-		var bar2TimeVar = timer.time($call);		
-		var logger = new Logger();
-		logger.text("I want to print the value of the elapsed time (")
-			.double(bar2TimeVar)
-			.text("), which is in the unit '" + timer.getUnit().getUnitsString() + "' and put other stuff after it")
-			.ln()
-			.log(timer.getAfterJp());
-	end
-	
-	// Enable printing again
-	timer.setPrint(true);
-	select type.executable.call{"bar3"} end
-	apply
-		timer.time($call);		
-		var logger = new Logger();
-		logger.text("This should appear after the timer print")
-			.ln()
-			.log(timer.getAfterJp());
-	end
-	
-	// Print code
-	select file end
-	apply
-		console.log($file.srcCode);
-	end
+    timer.time(call, "Time:");
+}
 
-end
+// Disable printing result
+timer.setPrint(false);
+
+for (const call of Query.search("type").search("executable").search("call", "bar2")) {
+    const bar2TimeVar = timer.time(call);
+    const logger = new Logger();
+    logger.text("I want to print the value of the elapsed time (")
+        .double(bar2TimeVar)
+        .text("), which is in the unit '" + timer.getUnit().getUnitsString() + "' and put other stuff after it")
+        .ln()
+        .log(timer.getAfterJp());
+}
+
+// Enable printing again
+timer.setPrint(true);
+
+for (const call of Query.search("type").search("executable").search("call", "bar3")) {
+    timer.time(call);
+    const logger = new Logger();
+    logger.text("This should appear after the timer print")
+        .ln()
+        .log(timer.getAfterJp());
+}
+
+console.log(Query.search("file").getFirst().srcCode);
