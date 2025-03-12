a9d587fee94e29f89f512eb21e30e5ec29414f0e
DiogoMartins888 <up202108883@up.pt>
2025-03-12 17:55:47 +0000

    [JavaWeaver] Remove unnecessary division in function calls

--- JavaWeaver/resources/kadabra/test/api/MutationsTest.js
+++ JavaWeaver/resources/kadabra/test/api/MutationsTest.js
@@ -3,35 +3,26 @@ laraImport("kadabra.mutation.ConditionalOperatorDeletionMutation");
 laraImport("lara.mutation.IterativeMutator");
 laraImport("weaver.Query");
 
-testArithmeticOperatorDeletionMutation();
-testConditionalOperatorDeletionMutation();
 
+console.log("ArithmeticOperatorDeletion");
 
-function testArithmeticOperatorDeletionMutation() {
+var mutator = new IterativeMutator(new ArithmeticOperatorDeletionMutation());
+mutator.addJps(Query.search("method", "arithmeticOperatorDeletion").first().descendants);
 
-    console.log("ArithmeticOperatorDeletion");
-
-    var mutator = new IterativeMutator(new ArithmeticOperatorDeletionMutation());
-    mutator.addJps(Query.search("method", "arithmeticOperatorDeletion").first().descendants);
-
-    // Generate all mutations
-    while (mutator.mutateSource()) {
-        // Print
-        console.log(mutator.getMutatedPoint().getAncestor('statement').srcCode);
-    }
+// Generate all mutations
+while (mutator.mutateSource()) {
+    // Print
+    console.log(mutator.getMutatedPoint().getAncestor('statement').srcCode);
 }
 
 
-function testConditionalOperatorDeletionMutation() {
+console.log("ConditionalOperatorDeletionMutation");
 
-    console.log("ConditionalOperatorDeletionMutation");
+var mutator = new IterativeMutator(new ConditionalOperatorDeletionMutation());
+mutator.addJps(Query.search("method", "conditionalOperatorDeletion").first().descendants);
 
-    var mutator = new IterativeMutator(new ConditionalOperatorDeletionMutation());
-    mutator.addJps(Query.search("method", "conditionalOperatorDeletion").first().descendants);
-
-    // Generate all mutations
-    while (mutator.mutateSource()) {
-        // Print
-        console.log(mutator.getMutatedPoint().srcCode);
-    }
+// Generate all mutations
+while (mutator.mutateSource()) {
+    // Print
+    console.log(mutator.getMutatedPoint().srcCode);
 }
