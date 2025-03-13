d02013ab3861026dda8d91917bdb62c73999fa89
DiogoMartins888 <up202108883@up.pt>
2025-03-13 15:51:07 +0000

    [JavaWeaver] Replace loop with break with getFirst()

--- JavaWeaver/resources/kadabra/test/weaver/GlobalJp.js
+++ JavaWeaver/resources/kadabra/test/weaver/GlobalJp.js
@@ -3,19 +3,17 @@ laraImport("weaver.Query");
 // App children
 const $app = Query.root();
 console.log("app num children: " + $app.numChildren);
-	
+
 // File children
-for(const $file of Query.search("file")){
-	console.log("file num children: " + $file.numChildren);	
+for (const $file of Query.search("file")) {
+    console.log("file num children: " + $file.numChildren);
 }
 
 // Statement children
-for(const $statement of Query.search("type").search("method","foo").search("statement")){
-	console.log("stmt children: " + $statement.children);
-	console.log("stmt child 0: " + $statement.child(0));
-	console.log("stmt num children: " + $statement.numChildren);
-	break;
-}
+const $statement = Query.search("type").search("method", "foo").search("statement").getFirst();
+console.log("stmt children: " + $statement.children);
+console.log("stmt child 0: " + $statement.child(0));
+console.log("stmt num children: " + $statement.numChildren);
 
 // App ast
 const $newApp = Query.root();
