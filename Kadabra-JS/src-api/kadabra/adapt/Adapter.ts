e28de59148febee6a4d45987eaead397d5a45b05
Antonio Lima <up202003386@up.pt>
2025-04-03 19:43:55 +0100

    adapter ts converted, cant test, kadabra.factory dependent

--- Kadabra-JS/src-api/kadabra/adapt/Adapter.ts
+++ Kadabra-JS/src-api/kadabra/adapt/Adapter.ts
@@ -1,107 +1,103 @@
 import kadabra.Factory;
-aspectdef CreateClassGenerator
-	input 
-		adapterMethod,
-		adapterClass=".*",
-		$interfaceMethod,
-		$storingClass
-	end
-	output  $adaptMethod, generate, generateQualified end
-	select file.class{name~=adapterClass}.method{adapterMethod} end
-	apply
+import Query from "@specs-feup/lara/api/weaver/Query.js";
+import { Class, FileJp, Method } from "../../Joinpoints.js";
+
+function CreateClassGenerator(adapterMethod :string, adapterClass:string, $interfaceMethod:Method, $storingClass:Class){
+	if(adapterClass == undefined) adapterClass ="*";
+
+	let $adaptMethod;
+	let generate;
+	let generateQualified
+
+
+	for(const $method of Query.search(FileJp).search(Class, { "name": (c) => c !== adapterClass }).search(Method,adapterMethod)){
 		if($adaptMethod != undefined){
 			throw 'More than one target method generator was found, please define a finer selection';
 		}
-		adapter = call FunctionGenerator($method,$interfaceMethod,$storingClass);
+		const adapter =FunctionGenerator($method,$interfaceMethod,$storingClass);
 		$adaptMethod= adapter.$adaptMethod;
 		generate= adapter.generate;
 		generateQualified = adapter.generateQualified;
-	end
-	if($adaptMethod == undefined){
-		throw 'Could not find given method generator: '+adapterMethod+" in "+adapter.class;
 	}
-end
+	if($adaptMethod == undefined){
+		throw 'Could not find given method generator: '+adapterMethod+" in "+adapterClass;
+	}
+	
+	return { $adaptMethod:$adaptMethod, generate, generateQualified};
+}
+function FunctionGenerator($adapterMethod:Method, $interfaceMethod:Method, $storingClass:Class){
 
-aspectdef FunctionGenerator
-	input $adapterMethod, $interfaceMethod, $storingClass end
-	output  $adaptMethod, generate, generateQualified end
 	if($storingClass == undefined){
-		creator = call GetOrNewClass("kadabra.utils.Adapters");
+		const creator =GetOrNewClass("kadabra.utils.Adapters");
 		$storingClass = creator.$class;
 	}
 	console.log("[FunctionGenerator] Creating new functional class with "+$interfaceMethod +" and "+$adapterMethod);
-	$adaptMethod = $storingClass.exec newFunctionalClass($interfaceMethod, $adapterMethod);
-	generate = function(){
+	const $adaptMethod = $storingClass.newFunctionalClass($interfaceMethod, $adapterMethod);
+	const generate = function(){
 		var invoke = $adaptMethod +"(";
 		var args = Array.prototype.slice.call(arguments);
 		invoke += args.join(", ");
 		invoke +=")";
 		return invoke;
 	  };
-	 generateQualified = function(){
+	 const generateQualified = function(){
 		var invoke = $storingClass.qualifiedName+"."+$adaptMethod +"(";
 		var args = Array.prototype.slice.call(arguments);
 		invoke += args.join(", ");
 		invoke +=")";
 		return invoke;
 	  };
-end
-
+	return {$adaptMethod:$adapterMethod, generate:generate, generateQualified:generateQualified};
+}
 /**
  *
  */
-aspectdef CreateAdapter
-	input 
-		target,
-		adapter,
-		targetClass=".*",
-		adapterClass=".*",
-		name
-	end
-	output  $adaptClass, addField end
-//	output
-//		adapter;
-//	end
-	targetMethod: select app.$ft=file.($target=class){name~=targetClass}.method{name==target} end
-	adaptMethod: select app.$fa=file.($adapter=class){name~=adapterClass}.($adaptMethod = method){name==adapter} end
-	apply to targetMethod::adaptMethod
-		if($adaptClass != undefined){
-			throw 'More than one target class/adapter method was found, please define a finer selection';
+function CreateAdapter(target:string, adapter:string, targetClass:string, adapterClass:string,name:string){
+		
+	let $adaptClass;
+	let addField;
+	const $target= Query.search(FileJp).search(Class, { "name": (c) => c !== targetClass});
+	const $adapter = Query.search(FileJp).search(Class, {"name": (c) => c !== adapterClass});
+	for (const $method of $target.search(Method, {"name" : target})){
+		for(const $adaptMethod of $adapter.search(Method, {"name": adapter})){
+			if($adaptClass != undefined){
+				throw 'More than one target class/adapter method was found, please define a finer selection';
+			}
+			name.charAt(0).toUpperCase()
+			name = name || $method.name.charAt(0).toUpperCase()+$method.name.substring(1)+"_"+adapter+"_"+$adaptMethod.name.charAt(0).toUpperCase()+$adaptMethod.name.substring(1)+"_Adapter";
+			let _adapter = TransformMethod($method, $adaptMethod, name);//, $target.name.firstCharToUpper()+"Adapter");
+			$adaptClass = _adapter.$adaptClass;
+			addField = _adapter.addField;
 		}
-		name = name || $method.name.firstCharToUpper()+"_"+$adapter.name+"_"+$adaptMethod.name.firstCharToUpper()+"_Adapter";
-		adapter = call TransformMethod($method, $adaptMethod, name);//, $target.name.firstCharToUpper()+"Adapter");
-		$adaptClass = adapter.$adaptClass;
-		addField = adapter.addField;
-	end
-end
-
+	}
+	return {$adaptClas: $adaptClass, addField: addField };
+}
 /**
  * Create an adapter based on the target class and the method that transforms the class bytecodes.
  * 
  */
-aspectdef TransformMethod
-	input $target, $adaptMethod, name end
-	output  $adaptClass, addField end //fieldName, $adaptField, addField, addAdapter, adapt,
-	name = name || $target.name.firstCharToUpper()+$adaptMethod.name.firstCharToUpper()+"Adapter";
-	select class{name} end
-	apply
+function TransformMethod($target: Method, $adaptMethod: Method, name:string){
+	name = name || $target.name.charAt(0).toUpperCase()+$target.name.substring(1)+$adaptMethod.name.charAt(0).toUpperCase+$adaptMethod.name.substring(1)+"Adapter";
+	
+	let $adaptClass :Class;
+	for(const $class of Query.search(FileJp).search(Class, {"name":name})){
 		$adaptClass = $class;
-	end
+	}
 	if($adaptClass == undefined){
 		try {
-		$adaptClass = $target.exec createAdapter($adaptMethod, name);
+		$adaptClass = $target.createAdapter($adaptMethod, name);
 		}catch(e){
 			e.printStackTrace();
 		}
 	}
 	//this method returns information regarding the field and class, as well as the methods that can be invoked in the field
-	addField = function($class, name, init){
-		var $class = $class || $adaptClass;
-		var fieldName = name || $adaptClass.name.firstCharToLower();
-		var init = init || false;
-		$newField = $class.exec newField(["public", "static"], $adaptClass.qualifiedName, fieldName, "new "+$adaptClass.name+"()");
+	const addField= ($class:Class, name:string, init:boolean): any=>{
+		let _$class = $class || $adaptClass;
+		const fieldName = name || $adaptClass.name.charAt(0).toUpperCase()+$adaptClass.name.substring(1);
+		const _init = init || false;
+		const $newField = _$class.newField(["public", "static"], $adaptClass.qualifiedName, fieldName, "new "+$adaptClass.name+"()");
 		
-		var field = {
+		let field = {
 			name: fieldName,
 			$field: $newField,
 			addAdapter: "weaver.kadabra.agent.AgentUtils.addAdapter("+fieldName+");",
@@ -114,9 +110,11 @@ aspectdef TransformMethod
 				  },
 		};
 		if(init){
-			$class.exec insertStatic %{[[field.addAdapter]]}%;
+			_$class.insertStatic(field.addAdapter);
 		}
 	
 		return field;
 	};
-end
+	return {$adaptClass:$adaptClass, addField:addField};//fieldName, $adaptField, addField, addAdapter, adapt,
+
+}
