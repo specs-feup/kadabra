5fdf1b0e87fcf4f4daf33ba7d35f098561f85617
Antonio Lima <up202003386@up.pt>
2025-04-04 15:32:50 +0100

    Changing lambda cases and make lambda a Method

--- Kadabra-JS/src-api/kadabra/adapt/Adapter.ts
+++ Kadabra-JS/src-api/kadabra/adapt/Adapter.ts
@@ -1,121 +1,193 @@
-import kadabra.Factory;
+//import kadabra.Factory;
 import Query from "@specs-feup/lara/api/weaver/Query.js";
 import { Class, FileJp, Method } from "../../Joinpoints.js";
 
-export function CreateClassGenerator(adapterMethod :string, adapterClass?:string, $interfaceMethod:Method, $storingClass:Class){
-	if(adapterClass == undefined) adapterClass ="*";
+export function CreateClassGenerator(
+    adapterMethod: string,
+    adapterClass: string,
+    $interfaceMethod: Method,
+    $storingClass: Class
+) {
+    if (adapterClass == undefined) adapterClass = "*";
 
-	let $adaptMethod;
-	let generate;
-	let generateQualified
+    let $adaptMethod;
+    let generate;
+    let generateQualified;
 
+    for (const $method of Query.search(FileJp)
+        .search(Class, { name: (c: string) => c !== adapterClass })
+        .search(Method, adapterMethod)) {
+        if ($adaptMethod != undefined) {
+            throw "More than one target method generator was found, please define a finer selection";
+        }
+        const adapter = FunctionGenerator(
+            $method,
+            $interfaceMethod,
+            $storingClass
+        );
+        $adaptMethod = adapter.$adaptMethod;
+        generate = adapter.generate;
+        generateQualified = adapter.generateQualified;
+    }
+    if ($adaptMethod == undefined) {
+        throw (
+            "Could not find given method generator: " +
+            adapterMethod +
+            " in " +
+            adapterClass
+        );
+    }
 
-	for(const $method of Query.search(FileJp).search(Class, { "name": (c) => c !== adapterClass }).search(Method,adapterMethod)){
-		if($adaptMethod != undefined){
-			throw 'More than one target method generator was found, please define a finer selection';
-		}
-		const adapter =FunctionGenerator($method,$interfaceMethod,$storingClass);
-		$adaptMethod= adapter.$adaptMethod;
-		generate= adapter.generate;
-		generateQualified = adapter.generateQualified;
-	}
-	if($adaptMethod == undefined){
-		throw 'Could not find given method generator: '+adapterMethod+" in "+adapterClass;
-	}
-	
-	return { $adaptMethod:$adaptMethod, generate:generate, generateQualified:generateQualified};
+    return {
+        $adaptMethod: $adaptMethod,
+        generate: generate,
+        generateQualified: generateQualified,
+    };
 }
-export function FunctionGenerator($adapterMethod:Method, $interfaceMethod:Method, $storingClass?:Class){
+export function FunctionGenerator(
+    $adapterMethod: Method,
+    $interfaceMethod: Method,
+    $storingClass?: Class
+) {
+    if ($storingClass == undefined) {
+        const creator = GetOrNewClass("kadabra.utils.Adapters");
+        $storingClass = creator.$class;
+    }
+    console.log(
+        "[FunctionGenerator] Creating new functional class with " +
+            $interfaceMethod +
+            " and " +
+            $adapterMethod
+    );
+    const $adaptMethod = $storingClass.newFunctionalClass(
+        $interfaceMethod,
+        $adapterMethod
+    );
+    const generate = function (args: string[][]) {
+        let invoke = $adaptMethod + "(";
+        const _args = args.slice();
+        invoke += _args.join(", ");
+        invoke += ")";
+        return invoke;
+    };
 
-	if($storingClass == undefined){
-		const creator =GetOrNewClass("kadabra.utils.Adapters");
-		$storingClass = creator.$class;
-	}
-	console.log("[FunctionGenerator] Creating new functional class with "+$interfaceMethod +" and "+$adapterMethod);
-	const $adaptMethod = $storingClass.newFunctionalClass($interfaceMethod, $adapterMethod);
-	const generate = function(args:string[][]){
-		var invoke = $adaptMethod +"(";
-		var _args = args.slice();
-		invoke += _args.join(", ");
-		invoke +=")";
-		return invoke;
-	  };
-
-	 const generateQualified = function(args:string[][]){
-		var invoke = $storingClass.qualifiedName+"."+$adaptMethod +"(";
-		var _args = args.slice();
-		invoke += _args.join(", ");
-		invoke +=")";
-		return invoke;
-	  };
-	return {$adaptMethod:$adapterMethod, generate:generate, generateQualified:generateQualified};
+    const generateQualified = function (args: string[][]) {
+        let invoke = $storingClass.qualifiedName + "." + $adaptMethod + "(";
+        const _args = args.slice();
+        invoke += _args.join(", ");
+        invoke += ")";
+        return invoke;
+    };
+    return {
+        $adaptMethod: $adapterMethod,
+        generate: generate,
+        generateQualified: generateQualified,
+    };
 }
 /**
  *
  */
-export function CreateAdapter(target:string, adapter:string, targetClass:string, adapterClass:string,name:string){
-		
-	let $adaptClass;
-	let addField;
-	const $target= Query.search(FileJp).search(Class, { "name": (c) => c !== targetClass});
-	const $adapter = Query.search(FileJp).search(Class, {"name": (c) => c !== adapterClass});
-	for (const $method of $target.search(Method, {"name" : target})){
-		for(const $adaptMethod of $adapter.search(Method, {"name": adapter})){
-			if($adaptClass != undefined){
-				throw 'More than one target class/adapter method was found, please define a finer selection';
-			}
-			name.charAt(0).toUpperCase()
-			name = name || $method.name.charAt(0).toUpperCase()+$method.name.substring(1)+"_"+adapter+"_"+$adaptMethod.name.charAt(0).toUpperCase()+$adaptMethod.name.substring(1)+"_Adapter";
-			let _adapter = TransformMethod($method, $adaptMethod, name);//, $target.name.firstCharToUpper()+"Adapter");
-			$adaptClass = _adapter.$adaptClass;
-			addField = _adapter.addField;
-		}
-	}
-	return {$adaptClass: $adaptClass, addField: addField };
+export function CreateAdapter(
+    target: string,
+    adapter: string,
+    targetClass: string,
+    adapterClass: string,
+    name: string
+) {
+    let $adaptClass;
+    let addField;
+    const $target = Query.search(FileJp).search(Class, {
+        name: (c) => c !== targetClass,
+    });
+    const $adapter = Query.search(FileJp).search(Class, {
+        name: (c) => c !== adapterClass,
+    });
+    for (const $method of $target.search(Method, { name: target })) {
+        for (const $adaptMethod of $adapter.search(Method, { name: adapter })) {
+            if ($adaptClass != undefined) {
+                throw "More than one target class/adapter method was found, please define a finer selection";
+            }
+            name.charAt(0).toUpperCase();
+            name =
+                name ||
+                $method.name.charAt(0).toUpperCase() +
+                    $method.name.substring(1) +
+                    "_" +
+                    adapter +
+                    "_" +
+                    $adaptMethod.name.charAt(0).toUpperCase() +
+                    $adaptMethod.name.substring(1) +
+                    "_Adapter";
+            const _adapter = TransformMethod($method, $adaptMethod, name); //, $target.name.firstCharToUpper()+"Adapter");
+            $adaptClass = _adapter.$adaptClass;
+            addField = _adapter.addField;
+        }
+    }
+    return { $adaptClass: $adaptClass, addField: addField };
 }
 /**
  * Create an adapter based on the target class and the method that transforms the class bytecodes.
- * 
+ *
  */
-export function TransformMethod($target: Method, $adaptMethod: Method, name?:string){
-	name = name || $target.name.charAt(0).toUpperCase()+$target.name.substring(1)+$adaptMethod.name.charAt(0).toUpperCase+$adaptMethod.name.substring(1)+"Adapter";
-	
-	let $adaptClass :Class;
-	for(const $class of Query.search(FileJp).search(Class, {"name":name})){
-		$adaptClass = $class;
-	}
-	if($adaptClass == undefined){
-		try {
-		$adaptClass = $target.createAdapter($adaptMethod, name);
-		}catch(e){
-			e.printStackTrace();
-		}
-	}
-	//this method returns information regarding the field and class, as well as the methods that can be invoked in the field
-	const addField= ($class?:Class, name?:string, init?:boolean): any=>{
-		let _$class = $class || $adaptClass;
-		const fieldName = name || $adaptClass.name.charAt(0).toUpperCase()+$adaptClass.name.substring(1);
-		const _init = init || false;
-		const $newField = _$class.newField(["public", "static"], $adaptClass.qualifiedName, fieldName, "new "+$adaptClass.name+"()");
-		
-		let field = {
-			name: fieldName,
-			$field: $newField,
-			addAdapter: "weaver.kadabra.agent.AgentUtils.addAdapter("+fieldName+");",
-			adapt: function(args:string[][]){
-			    		var invoke = $newField.staticAccess+".adapt(";
-			    		var _args = args.slice();
-			    		invoke += _args.join(", ");
-			    		invoke +=");";
-			    		return invoke;
-				  },
-		};
-		if(init){
-			_$class.insertStatic(field.addAdapter);
-		}
-	
-		return field;
-	};
-	return {$adaptClass:$adaptClass, addField:addField};//fieldName, $adaptField, addField, addAdapter, adapt,
+export function TransformMethod(
+    $target: Method,
+    $adaptMethod: Method,
+    name?: string
+) {
+    name =
+        name ||
+        $target.name.charAt(0).toUpperCase() +
+            $target.name.substring(1) +
+            $adaptMethod.name.charAt(0).toUpperCase +
+            $adaptMethod.name.substring(1) +
+            "Adapter";
 
+    let $adaptClass: Class;
+    for (const $class of Query.search(FileJp).search(Class, { name: name })) {
+        $adaptClass = $class;
+    }
+    if ($adaptClass == undefined) {
+        try {
+            $adaptClass = $target.createAdapter($adaptMethod, name);
+        } catch (e) {
+            e.printStackTrace();
+        }
+    }
+    //this method returns information regarding the field and class, as well as the methods that can be invoked in the field
+    const addField = ($class?: Class, name?: string, init?: boolean): any => {
+        const _$class = $class || $adaptClass;
+        const fieldName =
+            name ||
+            $adaptClass.name.charAt(0).toUpperCase() +
+                $adaptClass.name.substring(1);
+        const _init = init || false;
+        const $newField = _$class.newField(
+            ["public", "static"],
+            $adaptClass.qualifiedName,
+            fieldName,
+            "new " + $adaptClass.name + "()"
+        );
+
+        const field = {
+            name: fieldName,
+            $field: $newField,
+            addAdapter:
+                "weaver.kadabra.agent.AgentUtils.addAdapter(" +
+                fieldName +
+                ");",
+            adapt: function (...args: string[]) {
+                let invoke = $newField.staticAccess + ".adapt(";
+                const _args = args.slice();
+                invoke += _args.join(", ");
+                invoke += ");";
+                return invoke;
+            },
+        };
+        if (init) {
+            _$class.insertStatic(field.addAdapter);
+        }
+
+        return field;
+    };
+    return { $adaptClass: $adaptClass, addField: addField }; //fieldName, $adaptField, addField, addAdapter, adapt,
 }
--- Kadabra-JS/src-api/kadabra/adapt/Algorithm.ts
+++ Kadabra-JS/src-api/kadabra/adapt/Algorithm.ts
@@ -1,127 +1,189 @@
-import {TransformMethod,FunctionGenerator }from "./Adapter.js";
-import {GetMethod } from "../Utils.js";
+import { TransformMethod, FunctionGenerator } from "./Adapter.js";
+import { GetMethod } from "../Utils.js";
 
-import KadabraJavaTypes from "../KadabraJavaTypes.js";
 import { Method } from "../../Joinpoints.js";
 
-export abstract class Algorithm{
-	static const PACKAGE = 'autotuner.algorithm.';
-	static const PROVIDER_PACKAGE = this.PACKAGE +'.provider.';
+export abstract class Algorithm {
+    public static readonly PACKAGE = "autotuner.algorithm.";
+    public static readonly PROVIDER_PACKAGE = this.PACKAGE + ".provider.";
 
-	lambda:string;
-	id:string;
+    lambda: string;
+    id: string;
 
-	constructor(){}
-	abstract getAlgorithm():string;
+    abstract getAlgorithm(): string;
 
-	getSupplier():string {
-		return '()->'+this.getAlgorithm();
-	}
-
+    getSupplier(): string {
+        return "()->" + this.getAlgorithm();
+    }
 }
 
-export class SimpleAlgorithm extends Algorithm{
-	constructor(lambda: any, id?:string){
-		super();
-		if(lambda instanceof KadabraJavaTypes.JoinPoint){
-			this.lambda = lambda.toQualifiedReference;
-		}else{ //assume native lambda code 
-			this.lambda = lambda;
-		}
-		this.id = id || this.lambda;
-	//	this.isAlgorithm = true; //helper
-	}
+export class SimpleAlgorithm extends Algorithm {
+    constructor(lambda: string | Method, id?: string) {
+        super();
+        if (lambda instanceof Method) {
+            this.lambda = lambda.toQualifiedReference;
+        } else {
+            //assume native lambda code
+            this.lambda = lambda;
+        }
+        this.id = id ?? this.lambda;
+        //	this.isAlgorithm = true; //helper
+    }
 
-	
-	getAlgorithm():string{
-		return 'new '+Algorithm.PROVIDER_PACKAGE+'SingleAlgorithmProvider<>('+this.lambda+', "'+this.id+'")';
-	}
+    getAlgorithm(): string {
+        return (
+            "new " +
+            Algorithm.PROVIDER_PACKAGE +
+            "SingleAlgorithmProvider<>(" +
+            this.lambda +
+            ', "' +
+            this.id +
+            '")'
+        );
+    }
 
-	instance():string{
-		return 'new '+Algorithm.PACKAGE+'SimpleAlgorithm<>('+this.lambda+', "'+this.id+'")';
-	}
-
+    instance(): string {
+        return (
+            "new " +
+            Algorithm.PACKAGE +
+            "SimpleAlgorithm<>(" +
+            this.lambda +
+            ', "' +
+            this.id +
+            '")'
+        );
+    }
 }
 
-export class AlgorithmWithKnob extends Algorithm{
-	knobConsumer:any;
-	configSupplier:any;
+export class AlgorithmWithKnob extends Algorithm {
+    knobConsumer: any;
+    configSupplier: any;
 
-	constructor(lambda: any, id:any, knobConsumer:any, configSupplier:any){
-		super();
-		this.lambda = lambda;
-		this.id = id;
-		this.knobConsumer = knobConsumer;
-		this.configSupplier = configSupplier;
-	//	this.isAlgorithm = true; //helper
-	}
+    constructor(lambda: any, id: any, knobConsumer: any, configSupplier: any) {
+        super();
+        this.lambda = lambda;
+        this.id = id;
+        this.knobConsumer = knobConsumer;
+        this.configSupplier = configSupplier;
+        //	this.isAlgorithm = true; //helper
+    }
 
-	getAlgorithm():string{
-		return 'new '+Algorithm.PROVIDER_PACKAGE+'AlgorithmWithKnobProvider<>('+this.lambda+', "'+this.id+'",'+this.knobConsumer+','+this.configSupplier+')';
-	}
-
+    getAlgorithm(): string {
+        return (
+            "new " +
+            Algorithm.PROVIDER_PACKAGE +
+            "AlgorithmWithKnobProvider<>(" +
+            this.lambda +
+            ', "' +
+            this.id +
+            '",' +
+            this.knobConsumer +
+            "," +
+            this.configSupplier +
+            ")"
+        );
+    }
 }
 
-export class AdaptiveAlgorithm extends Algorithm{
-	provider:any;
-	
-	constructor(id, $targetMethod, templateName, provider){
-		super()
-		const getter = GetMethod(templateName);
-		const $templateMethod = getter.methods;
-	//	console.log($templateMethod);
-		const adapter = TransformMethod($targetMethod, $templateMethod);
-		const field = adapter.addField(undefined,id,true);
-		
-		//TODO check this: 
-		this.lambda = (k-> [[field.adapt("k")]] return [[$targetMethod.toQualifiedReference]];);
-		this.id = id;
-		this.provider = provider;
-	}
-	getAlgorithm():string{
-		return 'new '+Algorithm.PROVIDER_PACKAGE+'AdaptiveAlgorithmProvider<>('+this.lambda+', "'+this.id+'",'+this.provider+')';
-	}
+export class AdaptiveAlgorithm extends Algorithm {
+    provider: string;
+
+    constructor(id, $targetMethod, templateName, provider) {
+        super();
+        const getter = GetMethod(templateName);
+        const $templateMethod = getter.methods;
+        //	console.log($templateMethod);
+        const adapter = TransformMethod($targetMethod, $templateMethod);
+        const field = adapter.addField(undefined, id, true);
+
+        this.lambda = `k-> ${field.adapt("k")} return ${
+            $targetMethod.toQualifiedReference
+        };}`;
+
+        this.id = id;
+        this.provider = provider;
+    }
+    getAlgorithm(): string {
+        return (
+            "new " +
+            Algorithm.PROVIDER_PACKAGE +
+            "AdaptiveAlgorithmProvider<>(" +
+            this.lambda +
+            ', "' +
+            this.id +
+            '",' +
+            this.provider +
+            ")"
+        );
+    }
 }
 
-export class GenerativeAlgorithm extends Algorithm{
-	provider:string;
-	extraArg:string[];
-	providerType:any;
-	$interface:any;
-	constructor(id:string, $interface:Method, templateName:string, provider:string, providerType:any, extraArg?:string[]){
-		super();
-		const getter =GetMethod(templateName);
-		
-		const $templateMethod = getter.methods;	
-		if(Array.isArray($templateMethod )){
-			var getters = "";
-			for(var g of $templateMethod ){
-				getters+= g.toQualifiedReference+",";
-			}
-			throw 'Too much methods with template name: '+templateName+'. Origins: '+getters;
-		}
-		const adapter = FunctionGenerator($templateMethod, $interface);
-		//field = adapter.addField(undefined,id,true);
-		//TODO: check this
-		if(extraArg){
-			this.lambda = adapter.generateQualified([["k"], extraArg]);
-		}else{
-			this.lambda = adapter.generateQualified([["k"]]);
-		}
-		this.id = id;
-		this.provider = provider;
-		this.extraArg = extraArg;
-		this.providerType = providerType;
-		this.$interface = $interface;
-	}
-	getAlgorithm():string{
-		var genericType = '';
-		if(this.providerType!=undefined){
-			var interfType = this.$interface.getAncestor("interface").qualifiedName;
-			genericType =	interfType+","+this.providerType;
-		}
-		return 'new '+Algorithm.PROVIDER_PACKAGE+'GenerativeAlgorithmProvider<'+genericType+'>('+this.lambda+',"'+this.id+'",'+this.provider+')';
-	}
+export class GenerativeAlgorithm extends Algorithm {
+    provider: string;
+    extraArg: string[];
+    providerType: any;
+    $interface: any;
+    constructor(
+        id: string,
+        $interface: Method,
+        templateName: string,
+        provider: string,
+        providerType: any,
+        extraArg?: string[]
+    ) {
+        super();
+        const getter = GetMethod(templateName);
 
+        const $templateMethod = getter.methods;
+        if (Array.isArray($templateMethod)) {
+            let getters = "";
+            for (const g of $templateMethod) {
+                getters += g.toQualifiedReference + ",";
+            }
+            throw (
+                "Too much methods with template name: " +
+                templateName +
+                ". Origins: " +
+                getters
+            );
+        }
+        const adapter = FunctionGenerator($templateMethod, $interface);
+        //field = adapter.addField(undefined,id,true);
+        //TODO: check this
+        //before: this.lambda = %{k-> [[adapter.generateQualified("k", extraArg)]]}%;
+        //this.lambda = %{k-> [[adapter.generateQualified("k")]]}%;
 
+        if (extraArg) {
+            this.lambda = `k-> ${adapter.generateQualified("k", extraArg)}`;
+            adapter.generateQualified([["k"], extraArg]);
+        } else {
+            this.lambda = adapter.generateQualified([["k"]]);
+        }
+        this.id = id;
+        this.provider = provider;
+        this.extraArg = extraArg;
+        this.providerType = providerType;
+        this.$interface = $interface;
+    }
+    getAlgorithm(): string {
+        let genericType = "";
+        if (this.providerType != undefined) {
+            const interfType =
+                this.$interface.getAncestor("interface").qualifiedName;
+            genericType = interfType + "," + this.providerType;
+        }
+        return (
+            "new " +
+            Algorithm.PROVIDER_PACKAGE +
+            "GenerativeAlgorithmProvider<" +
+            genericType +
+            ">(" +
+            this.lambda +
+            ',"' +
+            this.id +
+            '",' +
+            this.provider +
+            ")"
+        );
+    }
 }
