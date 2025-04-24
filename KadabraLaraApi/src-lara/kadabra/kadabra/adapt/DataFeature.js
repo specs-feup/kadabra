9805ef0b182816588f9a4ccd1d50b3f8a8143433
DiogoMartins888 <up202108883@up.pt>
2025-04-24 10:50:57 +0100

    Trasnlate adapt/DataFeature

--- KadabraLaraApi/src-lara/kadabra/kadabra/adapt/DataFeature.js
+++ KadabraLaraApi/src-lara/kadabra/kadabra/adapt/DataFeature.js
@@ -1,29 +1,24 @@
-import { Class } from "../../Joinpoints.js";
 import { newClass } from "../Factory.js";
-
 export default class DataFeature {
-    static readonly PACKAGE = "kadabra.adapt.features";
-
-    name: string;
-    qualifiedName: string;
-    features: never[];
-    nc: Class | undefined;
-
-    constructor(name: string) {
+    static PACKAGE = "kadabra.adapt.features";
+    name;
+    qualifiedName;
+    features;
+    nc;
+    constructor(name) {
         this.name = name;
         this.qualifiedName = name;
         this.features = [];
         this.nc = undefined;
     }
-
-    generate(packageName?: string) {
+    generate(packageName) {
         packageName ??= DataFeature.PACKAGE;
         this.qualifiedName = packageName + "." + this.name;
         this.nc = newClass(this.qualifiedName, `Comparable<${this.name}>`);
         return this.nc;
     }
-
     addFeature() {
         return this.nc;
     }
 }
+//# sourceMappingURL=DataFeature.js.map
