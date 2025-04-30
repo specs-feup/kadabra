6703b98762b343b5f3add9eb007d5500cf86cf46
Antonio Lima <up202003386@up.pt>
2025-04-30 17:09:47 +0100

    adapter.ts changes

--- Kadabra-JS/src-api/kadabra/adapt/Adapter.ts
+++ Kadabra-JS/src-api/kadabra/adapt/Adapter.ts
@@ -1,6 +1,6 @@
-//import kadabra.Factory;
 import Query from "@specs-feup/lara/api/weaver/Query.js";
-import { Class, FileJp, Method, Field } from "../../Joinpoints.js";
+import { Class, Field, FileJp, Method } from "../../Joinpoints.js";
+import { getOrNewClass } from "../Factory.js";
 
 export function CreateClassGenerator(
     adapterMethod: string,
@@ -8,14 +8,12 @@ export function CreateClassGenerator(
     $interfaceMethod: Method,
     $storingClass: Class
 ) {
-    if (adapterClass == undefined) adapterClass = "*";
-
-    let $adaptMethod;
-    let generate;
-    let generateQualified;
+    let $adaptMethod: Method | undefined;
+    let generate: ((args: string[][]) => string) | undefined;
+    let generateQualified: ((args: string[][]) => string) | undefined;
 
     for (const $method of Query.search(FileJp)
-        .search(Class, { name: (c: string) => c !== adapterClass })
+        .search(Class, (c: Class) => RegExp(adapterClass).exec(c.name) !== null)
         .search(Method, adapterMethod)) {
         if ($adaptMethod != undefined) {
             throw new Error(
@@ -31,6 +29,7 @@ export function CreateClassGenerator(
         generate = adapter.generate;
         generateQualified = adapter.generateQualified;
     }
+
     if ($adaptMethod == undefined) {
         throw new Error(
             "Could not find given method generator: " +
@@ -50,41 +49,57 @@ export function FunctionGenerator(
     $adapterMethod: Method,
     $interfaceMethod: Method,
     $storingClass?: Class
-) {
+): {
+    $adaptMethod: Method;
+    generate: (args: string[][]) => string;
+    generateQualified: (args: string[][]) => string;
+} {
     if ($storingClass == undefined) {
-        const creator = GetOrNewClass("kadabra.utils.Adapters");
-        $storingClass = creator.$class;
+        const $class = getOrNewClass("kadabra.utils.Adapters");
+        $storingClass = $class;
     }
-    console.log(
-        "[FunctionGenerator] Creating new functional class with " +
-            $interfaceMethod +
-            " and " +
-            $adapterMethod
-    );
-    const $adaptMethod = $storingClass.newFunctionalClass(
-        $interfaceMethod,
-        $adapterMethod
-    );
-    const generate = function (args: string[][]) {
-        let invoke = $adaptMethod + "(";
-        const _args = args.slice();
-        invoke += _args.join(", ");
-        invoke += ")";
-        return invoke;
-    };
 
-    const generateQualified = function (args: string[][]) {
-        let invoke = $storingClass.qualifiedName + "." + $adaptMethod + "(";
-        const _args = args.slice();
-        invoke += _args.join(", ");
-        invoke += ")";
-        return invoke;
-    };
-    return {
-        $adaptMethod: $adapterMethod,
-        generate: generate,
-        generateQualified: generateQualified,
-    };
+    if ($storingClass !== undefined) {
+        console.log(
+            "[FunctionGenerator] Creating new functional class with " +
+                $interfaceMethod +
+                " and " +
+                $adapterMethod
+        );
+
+        const $adaptMethod = $storingClass.newFunctionalClass(
+            $interfaceMethod,
+            $adapterMethod
+        );
+        const generate = function (args: string[][]) {
+            let invoke = $adaptMethod + "(";
+            const _args = args.slice();
+            invoke += _args.join(", ");
+            invoke += ")";
+            return invoke;
+        };
+
+        const generateQualified = function (args: string[][]) {
+            let invoke = $storingClass.qualifiedName + "." + $adaptMethod + "(";
+            const _args = args.slice();
+            invoke += _args.join(", ");
+            invoke += ")";
+            return invoke;
+        };
+
+        return {
+            $adaptMethod: $adapterMethod,
+            generate: generate,
+            generateQualified: generateQualified,
+        };
+    } else {
+        throw new Error(
+            "[FunctionGenerator] Cant create new functional class with " +
+                $interfaceMethod +
+                " and " +
+                $adapterMethod
+        );
+    }
 }
 /**
  *
@@ -96,19 +111,15 @@ export function CreateAdapter(
     adapterClass: string,
     name: string
 ) {
-    let $adaptClass;
+    let $adaptClass: Class | undefined;
     let addField;
     const $methods = Query.search(FileJp)
-        .search(Class, {
-            name: (c) => c !== targetClass, //change to regex
-        })
+        .search(Class, (c: Class) => RegExp(targetClass).exec(c.name) !== null)
         .search(Method, { name: target })
         .get();
 
     for (const $adaptMethod of Query.search(FileJp)
-        .search(Class, {
-            name: (c) => c !== adapterClass,
-        })
+        .search(Class, (c: Class) => RegExp(adapterClass).exec(c.name) != null)
         .search(Method, { name: adapter })) {
         for (const $method of $methods) {
             if ($method.equals($adaptMethod)) {
@@ -118,16 +129,15 @@ export function CreateAdapter(
                     );
                 }
 
-                name =
-                    name ||
+                name ??=
                     $method.name.charAt(0).toUpperCase() +
-                        $method.name.substring(1) +
-                        "_" +
-                        adapter +
-                        "_" +
-                        $adaptMethod.name.charAt(0).toUpperCase() +
-                        $adaptMethod.name.substring(1) +
-                        "_Adapter";
+                    $method.name.substring(1) +
+                    "_" +
+                    adapter +
+                    "_" +
+                    $adaptMethod.name.charAt(0).toUpperCase() +
+                    $adaptMethod.name.substring(1) +
+                    "_Adapter";
 
                 const _adapter = TransformMethod($method, $adaptMethod, name); //, $target.name.firstCharToUpper()+"Adapter");
                 $adaptClass = _adapter.$adaptClass;
@@ -137,12 +147,6 @@ export function CreateAdapter(
     }
     return { $adaptClass: $adaptClass, addField: addField };
 }
-interface InterfaceField {
-    name: string;
-    $field: Field;
-    addAdapter: string;
-    adapt: (...args: string[]) => string;
-}
 /**
  * Create an adapter based on the target class and the method that transforms the class bytecodes.
  *
@@ -151,65 +155,84 @@ export function TransformMethod(
     $target: Method,
     $adaptMethod: Method,
     name?: string
-) {
-    name =
-        name ??
+): {
+    $adaptClass: Class;
+    addField: (
+        $class: Class | undefined,
+        name: string,
+        init?: boolean
+    ) => {
+        name: string;
+        $field: Field;
+        addAdapter: string;
+        adapt: (...args: string[]) => string;
+    };
+} {
+    name ??=
         $target.name.charAt(0).toUpperCase() +
-            $target.name.substring(1) +
-            $adaptMethod.name.charAt(0).toUpperCase +
-            $adaptMethod.name.substring(1) +
-            "Adapter";
+        $target.name.substring(1) +
+        $adaptMethod.name.charAt(0).toUpperCase +
+        $adaptMethod.name.substring(1) +
+        "Adapter";
 
-    let $adaptClass: Class;
+    let $adaptClass: Class | undefined;
     for (const $class of Query.search(FileJp).search(Class, { name: name })) {
         $adaptClass = $class;
     }
+
     if ($adaptClass == undefined) {
         try {
             $adaptClass = $target.createAdapter($adaptMethod, name);
         } catch (e) {
-            e.printStackTrace();
+            if (e instanceof Error) console.log(e.message);
         }
     }
-    //this method returns information regarding the field and class, as well as the methods that can be invoked in the field
-    const addField = (
-        $class?: Class,
-        name?: string,
-        init?: boolean
-    ): InterfaceField => {
-        const _$class = $class ?? $adaptClass;
-        const fieldName =
-            name ??
-            $adaptClass.name.charAt(0).toUpperCase() +
-                $adaptClass.name.substring(1);
-        const _init = init || false;
-        const $newField = _$class.newField(
-            ["public", "static"],
-            $adaptClass.qualifiedName,
-            fieldName,
-            "new " + $adaptClass.name + "()"
-        );
 
-        const field = {
-            name: fieldName,
-            $field: $newField,
-            addAdapter:
-                "weaver.kadabra.agent.AgentUtils.addAdapter(" +
-                fieldName +
-                ");",
-            adapt: function (...args: string[]) {
-                let invoke = $newField.staticAccess + ".adapt(";
-                const _args = args.slice();
-                invoke += _args.join(", ");
-                invoke += ");";
-                return invoke;
-            },
+    if ($adaptClass != undefined) {
+        //this method returns information regarding the field and class, as well as the methods that can be invoked in the field
+        const addField = (
+            $class: Class | undefined,
+            name: string,
+            init?: boolean
+        ) => {
+            $class ??= $adaptClass;
+            const fieldName =
+                name ||
+                $adaptClass.name.charAt(0).toLowerCase() +
+                    $adaptClass.name.substring(1);
+            init ??= false;
+            const $newField = $class.newField(
+                ["public", "static"],
+                $adaptClass.qualifiedName,
+                fieldName,
+                "new " + $adaptClass.name + "()"
+            );
+
+            const field = {
+                name: fieldName,
+                $field: $newField,
+                addAdapter:
+                    "weaver.kadabra.agent.AgentUtils.addAdapter(" +
+                    fieldName +
+                    ");",
+                adapt: (...args: string[]) => {
+                    let invoke = $newField.staticAccess + ".adapt(";
+                    const _args = args.slice();
+                    invoke += _args.join(", ");
+                    invoke += ");";
+                    return invoke;
+                },
+            };
+
+            if (init) {
+                $class.insertStatic(`${field.addAdapter}`);
+            }
+
+            return field;
         };
-        if (_init) {
-            _$class.insertStatic(field.addAdapter);
-        }
 
-        return field;
-    };
-    return { $adaptClass: $adaptClass, addField: addField }; //fieldName, $adaptField, addField, addAdapter, adapt,
+        return { $adaptClass: $adaptClass, addField: addField };
+    } else {
+        throw new Error("[TransformMethod] cant create $adaptClass");
+    }
 }
