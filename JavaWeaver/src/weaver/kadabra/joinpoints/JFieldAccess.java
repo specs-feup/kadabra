f892c83be066c40adf1141755bf3d48ba1c89f72
João Bispo <jbispo@fe.up.pt>
2026-01-08 23:46:39 +0000

    [JavaWeaver] Removes override for 'name' in JFieldAccess, added new attribute 'base'

--- JavaWeaver/src/weaver/kadabra/joinpoints/JFieldAccess.java
+++ JavaWeaver/src/weaver/kadabra/joinpoints/JFieldAccess.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2015 SPeCS.
- * 
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * <p>
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License. under the License.
@@ -15,7 +15,9 @@ package weaver.kadabra.joinpoints;
 
 import spoon.reflect.code.CtFieldAccess;
 import spoon.reflect.declaration.CtElement;
+import weaver.kadabra.abstracts.joinpoints.AExpression;
 import weaver.kadabra.abstracts.joinpoints.AFieldAccess;
+import weaver.utils.weaving.converters.CtElement2JoinPoint;
 
 public class JFieldAccess<T> extends AFieldAccess {
 
@@ -36,8 +38,8 @@ public class JFieldAccess<T> extends AFieldAccess {
     }
 
     @Override
-    public String getNameImpl() {
-        return node.toString();
+    public AExpression getBaseImpl() {
+        return CtElement2JoinPoint.convert(node.getTarget(), AExpression.class);
     }
 
     // @Override
